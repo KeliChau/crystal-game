f5d8ca25f25cc1a2cfae2805cd37e84f362e6cf2
Keli Chau <inquiries@kelichau.com>
2017-11-27 19:54:47 -0500

    Added crystal number between 1 and 12

--- assets/javascript/crystal.js
+++ assets/javascript/crystal.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
+	//Random number at beginning between 19 and 120
 	var Random=Math.floor(Math.random()*101+19)
 	$('#randomNumber').text(Random);
 
-
-
+	//Random numbers for crystals between 1 and 12
+	var num1=Math.floor(Math.random()*11+1)
+	var num2=Math.floor(Math.random()*11+1)
+	var num3=Math.floor(Math.random()*11+1)
+	var num4=Math.floor(Math.random()*11+1)
 
 
 
