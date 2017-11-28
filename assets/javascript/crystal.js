7142cbaf693e089ebbca90c23f0c34e7202793f7
Keli Chau <inquiries@kelichau.com>
2017-11-27 20:00:31 -0500

    Added clicks for red crystal to test

--- assets/javascript/crystal.js
+++ assets/javascript/crystal.js
@@ -9,10 +9,26 @@ $(document).ready(function(){
 	var num3=Math.floor(Math.random()*11+1)
 	var num4=Math.floor(Math.random()*11+1)
 
+	//Score variables
+	var userScore=0;
+	var wins=0;
+	var losses=0;
 
+	$('#numberOfWins').text(wins);
+	$('#numberOfLosses').text(losses);
 
-
-
+	//Crystal clicks
+	$('#red').on('click', function(){
+		userScore=userScore + num1;
+		console.log("New userScore=" + userScore);
+		$('#totalScore').text(userScore);
+			if(userScore === Random){
+				win();
+			}
+			else if (userScore > Random){
+				lose();
+			}
+	})
 
 
 });
